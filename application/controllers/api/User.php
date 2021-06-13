5f64efa88babc0becf997e756dbad8f7618f64bd
--unset <--unset>
2021-06-13 19:12:15 +0700

    tambah kolom address

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -1,20 +1,24 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
+
 use Restserver\Libraries\REST_Controller;
 
-class User extends REST_Controller {
+class User extends REST_Controller
+{
 
-    function __construct($config = 'rest') {
+    function __construct($config = 'rest')
+    {
         parent::__construct($config);
         $this->load->database();
     }
 
     //Menampilkan data kontak
-    function index_get() {
+    function index_get()
+    {
         $id = $this->get('id');
         if ($id == '') {
             $kontak = $this->db->get('user')->result();
@@ -24,11 +28,14 @@ class User extends REST_Controller {
         }
         $this->response($kontak, 200);
     }
-    function index_post() {
+    function index_post()
+    {
         $data = array(
-                    'user'           => $this->post('user'),
-                    'password'          => $this->post('password'),
-                    'level'    => $this->post('level'));
+            'user'           => $this->post('user'),
+            'password'       => $this->post('password'),
+            'level'          => $this->post('level'),
+            'address'        => $this->post('address')
+        );
         $insert = $this->db->insert('user', $data);
         if ($insert) {
             $this->response($data, 200);
@@ -36,14 +43,16 @@ class User extends REST_Controller {
             $this->response(array('status' => 'fail', 502));
         }
     }
-    	//Memperbarui data kontak yang telah ada
-	function index_put() {
+    //Memperbarui data kontak yang telah ada
+    function index_put()
+    {
         $id = $this->put('id');
         $data = array(
             'id'           => $this->put('id'),
-            'user'           => $this->put('user'),
-            'password'          => $this->put('password'),
-            'level'    => $this->put('level'));
+            'user'         => $this->put('user'),
+            'level'        => $this->put('level'),
+            'address'      => $this->put('address')
+        );
         $this->db->where('id', $id);
         $update = $this->db->update('user', $data);
         if ($update) {
@@ -52,7 +61,8 @@ class User extends REST_Controller {
             $this->response(array('status' => 'fail', 502));
         }
     }
-    function index_delete() {
+    function index_delete()
+    {
         $id = $this->delete('id');
         $this->db->where('id', $id);
         $delete = $this->db->delete('user');
@@ -62,9 +72,8 @@ class User extends REST_Controller {
             $this->response(array('status' => 'fail', 502));
         }
     }
-	
+
 
 
     //Masukan function selanjutnya disini
 }
-?>
